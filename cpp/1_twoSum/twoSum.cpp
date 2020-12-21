60decb8d3cbf0f6d8c614a07fba609088bbef899
caliyang <55225028+caliyang@users.noreply.github.com>
2020-12-21 10:57:04 +0800

    Update twoSum.cpp

--- cpp/1_twoSum/twoSum.cpp
+++ cpp/1_twoSum/twoSum.cpp
@@ -15,68 +15,23 @@
 *               
 **********************************************************************************/
 
-
+//循环遍历，O(n^2)
 class Solution {
 public:
-    /*
-     *   The easy solution is O(n^2) run-time complexity.
-     *   ```
-     *       foreach(item1 in array) {
-     *           foreach(item2 in array){
-     *               if (item1 + item2 == target) {
-     *                   return result
-     *               }
-     *           }
-     *   ```
-     *   
-     *   We can see the nested loop just for searching, 
-     *   So, we can use a hashmap to reduce the searching time complexity from O(n) to O(1)
-     *   (the map's `key` is the number, the `value` is the position)
-     *   
-     *   But be careful, if there are duplication numbers in array, 
-     *   how the map store the positions for all of same numbers?
-     *
-     */
-
-
-    //
-    // The implementation as below is bit tricky. but not difficult to understand
-    //
-    //  1) Traverse the array one by one
-    //  2) just put the `target - num[i]`(not `num[i]`) into the map
-    //     so, when we checking the next num[i], if we found it is exisited in the map.
-    //     which means we found the second one.
-    //      
-    vector<int> twoSum(vector<int> &numbers, int target) {
-        unordered_map<int, int> m;
-        vector<int> result;
-        for(int i=0; i<numbers.size(); i++){
-            // not found the second one
-            if (m.find(numbers[i])==m.end() ) { 
-                // store the first one poisition into the second one's key
-                m[target - numbers[i]] = i; 
-            }else { 
-                // found the second one
-                result.push_back(m[numbers[i]]+1);
-                result.push_back(i+1);
-                break;
-            }
-        }
-        return result;
-    }
-
-    // we also can store nums[i] into map, and find target - nums[i]
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> m;
-        vector<int> result;
-        for (int i=0; i<nums.size(); i++) {
-            if ( m.find(target - nums[i]) == m.end() ) {
-                m[nums[i]] = i;
-            }else{
-                result.push_back(m[target - nums[i]]);
-                result.push_back(i);
+        int numssiz = nums.size();
+        for(int i1 = 0; i1 < numssiz; ++i1) {
+            for(int i2 = i1 + 1; i2 < numssiz; ++i2) {
+                if(nums[i1] + nums[i2] == target)
+                return {i1, i2};
             }
         }
-        return result;
+        cout << "给定值有误" << endl;
+        return {};
     }
 };
+
+/* 说明：
+ * 1.用 return语句 终止程序，而不是 break语句 跳出循环；
+ * 2.return语句返回值，不需要构造一个临时对象（类似 vector<int>({i1, i2})），直接用{i1, i2}即可；
+ */
