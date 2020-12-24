541c4a218a3a8df86d756aa0488015167c6799ee
caliyang <55225028+caliyang@users.noreply.github.com>
2020-12-24 16:42:54 +0800

    Update 3Sum.cpp

--- cpp/15_3Sum/3Sum.cpp
+++ cpp/15_3Sum/3Sum.cpp
@@ -13,5 +13,33 @@
 
 /**********************************************************************************
 * 说明：
-* 1.感觉类似于小学奥数题。。。
+* 1.不可以包含重复的三元组：一是需要排序，从小到大，且a<=b<=c；
+*                         二是对于某一重循环而言，相邻两次枚举的元素不能相同；
+* 2.双指针使用说明：排序后，蛮力枚举中的第三重循环可以与第二重循环并列进行，
+*   也就是说,保持第二重循环不变，而将第三重循环变成一个从数组最右端开始向左移动的指针,
+*   满足“枚举数组中两个元素时，随着第一个元素的递增，第二个元素是递减的”，因此可用双指针；
 **********************************************************************************/
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int numssiz = nums.size();
+        vector<vector<int>> answer;
+        sort(nums.begin(), nums.end());
+        for(int first = 0; first < numssiz - 2; ++first){
+            if(first > 0 &&  nums[first - 1] == nums[first])  continue;
+            int target = -nums[first];
+            int third = numssiz - 1;
+            for(int second = first + 1; second < numssiz - 1; ++second){
+                if(second > first + 1 && nums[second] == nums[second - 1])  continue;
+                while(second < third && nums[second] + nums[third] > target){
+                    --third;
+                }
+            if(second == third)  break;
+            if(nums[second] + nums[third] == target)  
+                answer.push_back({nums[first], nums[second], nums[third]});
+            }
+        }
+
+        return answer;
+    }
+};
