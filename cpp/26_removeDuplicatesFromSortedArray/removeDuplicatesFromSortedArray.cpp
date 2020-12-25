9ac1c9d7e2898df5b0b54f9e6c7a42491b2d6ca3
caliyang <55225028+caliyang@users.noreply.github.com>
2020-12-25 19:55:07 +0800

    Update removeDuplicatesFromSortedArray.cpp

--- cpp/26_removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.cpp
+++ cpp/26_removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.cpp
@@ -10,3 +10,23 @@
 * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。 
 *               
 **********************************************************************************/
+
+//方法：快慢指针；
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int length = nums.size();
+        if(length == 0) return 0;
+        int slow = 0;
+        for(int fast = 1; fast < length; fast++) {
+            if(nums[fast] != nums[fast - 1]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
+
+        return slow + 1;
+    }
+};
+
+//说明：注意首先验证数组的长度是否为0；
